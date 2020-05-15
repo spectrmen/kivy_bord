f5c4ce71675c0ecab03560b6b6411fac261d7754
spectrmen <spectrmen123@gmail.com>
2020-05-15 16:17:19 +0300

    changes

--- BordsGenerator.py
+++ BordsGenerator.py
@@ -1,8 +1,9 @@
 from kivy.app import App
+from itertools import zip_longest
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.graphics import (Color, Rectangle)
+from kivy.graphics import (Color, Rectangle, RoundedRectangle)
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -13,6 +14,33 @@ from fuzzywuzzy import process
 import csv
 
 class BordObject(Widget):
+    class NumberHome(TextInput):
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.LIMIT = 5
+            self.size_hint=(None, None)
+            self.background_color=(0,0,255,1)
+            self. multiline=False
+            self.font_size='90dp'
+            self.foreground_color=(255,255,255,1)
+        def insert_text(self, text, from_undo=False):
+            if len(self.text)>self.LIMIT:
+                return False
+            return super().insert_text(text, from_undo=from_undo)
+        def update_font(self):
+            if len(self.text) <= 2:
+                self.font_size = '100dp'
+            elif len(self.text) == 3:
+                self.font_size = '70dp'
+            elif len(self.text) == 4:
+                self.font_size = '60dp'
+            elif len(self.text) == 5:
+                self.font_size = '50dp'
+            elif len(self.text) == 6:
+                self.font_size = '40dp'
+        def update_padding(self, *args):
+            self.padding_y = self.height / 2 - (self.line_height / 2)
+
     def __init__(self, **kwards):
         super().__init__(**kwards)
         #загружаем улицы в объект
@@ -22,67 +50,73 @@ class BordObject(Widget):
             for row in csv.reader(f):
                     self.streets.append(tuple(row))
         self.only_streers = list(map(lambda x: x[0], self.streets))
+        self.only_prefix = list(map(lambda x: x[1], self.streets))
         #Создаем лебл
+        self.TOP_SHIFT = 20
+        self.SHIFT_HEADER = 70
+        self.rect = RoundedRectangle()
+        self.home_number =  self.number_home()
         self.lbl = Label(
                     text= self.streets[0][0],
                     font_size='100dp',
                     size_hint=(None, None),
+                    color=(255,255,255,1),
+                    padding=[50, 20]
+                    )
+        self.header = Label(
+                    text= self.streets[0][1],
+                    font_size='50dp',
+                    size_hint=(None, None),
                     color=(255,255,255,1))
         self.render()
         self.lbl.bind(text=self.update_all) #связка с полем lbl
-        #Clock.schedule_interval(self.callevery, 1)
-
-    def get_street(self,text:str):
-        street = process.extractOne(text, self.only_streers)
-        return str(street[0])
+        self.home_number.bind(text=self.update_num)
 
     def render(self):
         self.canvas.clear()
         print( 'i working in render %s' % self.lbl.size )
-        self.rect = Rectangle()
         '''создание номера'''
-        self.lbl.canvas.before.add(Color(255, 255, 255, 1))
-        self.lbl.canvas.before.add(self.rect)
-        self.lbl.canvas.ask_update()
-        self.create_num()
+        self.canvas.before.add(Color(255, 255, 255, 1))
+        self.canvas.before.add(self.rect)
+        self.canvas.ask_update()
         self.add_widget(self.lbl)
-
-    def create_num(self):
-        self.border_num = Rectangle()
-        self.home_number =  TextInput(size_hint=(None, None),
-                                          background_color=(0,0,255,1),
-                                          multiline=False,
-                                          font_size='90dp',
-                                          foreground_color=(255,255,255,1))
-        #self.home_number.canvas.before.add(Color(255,255,255,1))
-        #self.home_number.canvas.before.add(self.border_num)
-        #self.home_number.canvas.ask_update()
+        self.add_widget(self.header)
         self.add_widget(self.home_number)
+    @classmethod
+    def number_home(Class):
+        return Class.NumberHome()
     '''динамика'''
     def update_all(self, *args):
         self.update_lbl()
         self.update_rect()
         self.update_num()
+        self.update_header()
 
     def update_lbl(self, *args):
-        self.lbl.text = self.get_street(self.lbl.text)
+        self.lbl.text = process.extractOne(self.lbl.text, self.only_streers)[0]
         self.lbl._label.refresh()
         self.lbl.size = self.lbl._label.texture.size
         self.lbl.pos = (self.center_x-self.lbl.size[0]/2,
                          self.center_y-self.lbl.size[1]/2)
+    def update_header(self, *args):
+        self.header.text = self.only_prefix[self.only_streers.
+                                            index(self.lbl.text)]
+        self.header._label.refresh()
+        self.header.size = self.lbl._label.texture.size
+        self.header.pos = (self.center_x-self.lbl.size[0]/2,
+                         self.center_y-self.lbl.size[1]/2 + self.SHIFT_HEADER)
 
     def update_rect(self, *args):
         self.rect.size = self.lbl.size
-        self.rect.pos = self.lbl.pos
-        print( 'i working %s' % self.lbl.size )
+        self.rect.pos = [self.lbl.pos[0], self.lbl.pos[1]+self.TOP_SHIFT]
 
     def update_num(self, *args):
         self.home_number.size = (self.lbl.size[1], self.lbl.size[1])
         self.home_number.pos = (self.lbl.pos[0]+self.lbl.size[0],
-                                 self.lbl.pos[1])
-        self.border_num.size = (self.home_number.size[0]+2,
-                                self.home_number.size[1]+2)
-        self.border_num.pos = self.home_number.pos
+                                 self.lbl.pos[1]+self.TOP_SHIFT)
+        self.home_number.update_font()
+        self.home_number.update_padding()
+
     def clear_canvas(self, instance):
         self.canvas.clear()
 
@@ -91,9 +125,11 @@ class BordObject(Widget):
 
 class bordApp(App):
     def build(self):
-        #Window.clearcolor = (100, 0, 0, 1)
+
+        Window.clearcolor = (100, 0, 0, 1)
         parent = BoxLayout(orientation='vertical')
         self.painter = BordObject(size=(Window.size[0], Window.size[1]))
+        print(dir(self.painter))
         self.textinput = TextInput(text='введите название улицы',font_size = 50,
                       size_hint_y=None,
                       height=100,
@@ -104,9 +140,6 @@ class bordApp(App):
 
         blh.add_widget(Button(text = 'Save', on_press=self.painter.save,
                                  size=(100,50)))
-        '''blh.add_widget(Button(text = 'draw',
-                                  on_press=self.painter.render,
-                                 size=(100,50), pos=(100,0)))'''
         blh.add_widget(Button(text='Clear',
                                   on_press=self.painter.clear_canvas,
                                  size=(100,50), pos=(200,0)))
